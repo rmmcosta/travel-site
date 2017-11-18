841e760c08e5b56aa12dc26d14798ec8676d2a00
Ricardo Costa <ricardocosta101085@gmail.com>
2017-11-18 15:45:34 +0000

    gulp watch css

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,16 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function(){
-   console.log("Imagine Sass or PostCSS tasks running here."); 
+   //ass or PostCSS tasks running here
+   return gulp.src('app/assets/styles/styles.css').pipe(gulp.dest('app/temp/styles')); 
 });
 
 gulp.task('watch', function() {
-   watch('./app/index.html', function() {
+   watch('app/index.html', function() {
        gulp.start('html');
    }); 
    
-   watch('app/styles/**/*.css', function(){
+   watch('app/assets/styles/**/*.css', function(){
         gulp.start('styles');
    });
 });
