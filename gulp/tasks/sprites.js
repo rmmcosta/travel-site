33cdb1e4d46ea99f691ae3a90a2ee9a07f9b2541
Ricardo Costa <ricardocosta101085@gmail.com>
2017-12-17 14:37:16 +0000

    More on sprites with gulp

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp"),
-    svgSprite = require("gulp-svg-sprite");
+    svgSprite = require("gulp-svg-sprite"),
+    rename = require("gulp-rename");
 
 var config = {
     mode: {
         css: {
+            sprite: "sprite.svg",
             render: {
                 css: {
                     template: "./gulp/templates/sprite.css"
@@ -13,8 +15,23 @@ var config = {
     }
 }
 
-gulp.task('sprites', function () {
-    return gulp.src('./app/assets/images/icons/**/*.svg')
+gulp.task("createSprites", function () {
+    return gulp.src("./app/assets/images/icons/**/*.svg")
         .pipe(svgSprite(config))
         .pipe(gulp.dest("./app/temp/sprite"));
 });
+
+gulp.task("copySpriteSVG", ["createSprites"], function(){
+    return gulp.src("./app/temp/sprite/css/**/*.svg")
+        .pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+gulp.task("copySpriteCSS", ["createSprites"], function(){
+    return gulp.src("./app/temp/sprite/css/**/*.css")
+        .pipe(rename(function(path){
+            path.basename = "_" + path.basename;
+        }))
+        .pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("generateSprites", ["createSprites", "copySpriteSVG", "copySpriteCSS"]);
