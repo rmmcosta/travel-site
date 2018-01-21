e41db58e69c8f88d1a8b0899ac5608bd27c9d642
Ricardo Costa <ricardocosta101085@gmail.com>
2018-01-21 17:24:25 +0000

    Mobile Menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,3 @@
-import {Person, Baby, Adult} from './modules/Person';
+import MobileMenu from "./modules/MobileMenu";
 
-const p1 = new Adult("Ana", "Green");
-const p2 = new Baby("Ricardo", "Red");
-
-p1.print();
-p2.print();
-
-p2.cry();
-
-// Basic literal string creation
-let x = `This is a pretty little template string.`;
-
-// Multiline strings
-let y = `In ES5 this is
- not legal.`;
-
-// Interpolate variable bindings
-var name = "Bob", time = "today";
-console.log(`Hello ${name}, how are you ${time}?`);
+var mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,24 @@
+import $ from "jquery";
+
+class MobileMenu  {
+    constructor () {
+        this.pageHeader = $(".page-header");
+        this.menuIcon = $(".page-header__menu-icon");
+        this.menuLinks = $(".page-header__menu-container");
+        this.pNav = $(".primary-nav");
+        this.events();
+    }
+
+    events() {
+        this.menuIcon.click(this.toggleMenu.bind(this)); //bind is used to change the this context inside the called function
+    }
+
+    toggleMenu(){
+        this.menuLinks.toggleClass("page-header__menu-container--is-visible");
+        this.pageHeader.toggleClass("page-header--is-expanded");
+        this.pNav.toggleClass("primary-nav--pull-right");
+        this.menuIcon.toggleClass("page-header__menu-icon--close-x");
+    }
+}
+
+export default MobileMenu;
